b2693597b2f92bd3ac4e0e3e3f527ea72e212f71
datochakhunashvili <130147418+datochakhunashvili@users.noreply.github.com>
2023-05-21 00:08:55 +0400

    add. script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var firstName = "Dato";
+var lastName = "Chakhunashvili";
+var age = 40;
+var married = true
+console.log("my name is " + firstName + " " + lastName + " " + " and I am " + age + " " + " years old "  + " I am married " + married);
+console.log(`my name is ${firstName} ${lastName} and I am ${age} years old I am married: ${married}`);
