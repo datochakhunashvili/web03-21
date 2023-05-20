c08b720ef66a89838633029d22e5af022bc2200a
datochakhunashvili <130147418+datochakhunashvili@users.noreply.github.com>
2023-05-21 00:42:50 +0400

    add. js addition, division

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ var age = 40;
 var married = true
 console.log("my name is " + firstName + " " + lastName + " " + " and I am " + age + " " + " years old "  + " I am married " + married);
 console.log(`my name is ${firstName} ${lastName} and I am ${age} years old I am married: ${married}`);
+let x = 50;
+let y = 10;
+console.log(x+y);
+console.log(x-y);
+console.log(x/y);
+console.log(x*y);
