9872e35ffd48b0f1ddadc3ea7c7b62056302e2c9
datochakhunashvili <130147418+datochakhunashvili@users.noreply.github.com>
2023-05-23 23:39:48 +0400

    add. js. number

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-var firstName = "Dato";
-var lastName = "Chakhunashvili";
-var age = 40;
-var married = true
-console.log("my name is " + firstName + " " + lastName + " " + " and I am " + age + " " + " years old "  + " I am married " + married);
-console.log(`my name is ${firstName} ${lastName} and I am ${age} years old I am married: ${married}`);
-let x = 50;
-let y = 10;
-console.log(x+y);
-console.log(x-y);
-console.log(x/y);
-console.log(x*y);
-let x = 7*2;
-let y = 5*2;
-console.log(x+y);
+// var firstName = "Dato";
+// var lastName = "Chakhunashvili";
+// var age = 40;
+// var married = true
+// console.log("my name is " + firstName + " " + lastName + " " + " and I am " + age + " " + " years old "  + " I am married " + married);
+// console.log(`my name is ${firstName} ${lastName} and I am ${age} years old I am married: ${married}`);
+// let x = 50;
+// let y = 10;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x/y);
+// console.log(x*y);
+// let x = 7*2;
+// let y = 5*2;
+// console.log(x+y);
 
+
+let number1 = {
+    Number: 20,
+};
+let number2 = {
+    Number: 45, 
+};
+let number3 = {
+    Number: 22,
+};
+let number4 = {
+    Number: 35,
+};
+let number5 = {
+    Number: 50,
+};
+
+let numbers =[number1, number2, number3, number4, number5];
+
+console. log(number1 + number2 + number3 + number4 + number5);
